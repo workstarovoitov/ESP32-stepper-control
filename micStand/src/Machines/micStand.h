28b1e24f1aae80ae6b8603e97a601d699def96e8
Sergei Starovoitov <57865301+workstarovoitov@users.noreply.github.com>
2021-01-24 16:11:54 +0300

    Add files via upload

---
+++ micStand/src/Machines/micStand.h
@@ -0,0 +1,43 @@
+#pragma once
+// clang-format off
+
+/*
+    micStand.h
+    Part of Grbl_ESP32
+
+    Pin assignments for the ESP32 Development Controller, v4.1 and later.
+    https://github.com/bdring/Grbl_ESP32_Development_Controller
+    https://www.tindie.com/products/33366583/grbl_esp32-cnc-development-board-v35/
+
+    2018    - Bart Dring
+    2020    - Mitch Bradley
+
+    Grbl_ESP32 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Grbl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Grbl_ESP32.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define MACHINE_NAME            "MIC_STAND"
+
+#define X_STEP_PIN              GPIO_NUM_33
+#define X_DIRECTION_PIN         GPIO_NUM_32
+#define Y_STEP_PIN              GPIO_NUM_26
+#define Y_DIRECTION_PIN         GPIO_NUM_25
+#define Z_STEP_PIN              GPIO_NUM_14
+#define Z_DIRECTION_PIN         GPIO_NUM_27
+
+#define X_LIMIT_PIN             GPIO_NUM_36
+#define Y_LIMIT_PIN             GPIO_NUM_39
+//#define Z_LIMIT_PIN             GPIO_NUM_34
+
+// OK to comment out to use pin for other features
+#define STEPPERS_DISABLE_PIN    GPIO_NUM_15
